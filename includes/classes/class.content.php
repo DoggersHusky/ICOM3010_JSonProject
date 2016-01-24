311edecb00101620c6293caeb85ebf8ece73b6ec
DoggersHusky <w0227962@nscc.ca>
2016-01-24 15:11:43 -0400

    class and code cleanup
    class has been stripped serveral lins of code
    content class generates content from database automatically

--- includes/classes/class.content.php
+++ includes/classes/class.content.php
@@ -10,27 +10,13 @@
 		
 		public function content($query) {
 			$results = $this->query($query);
+			$json = array();
 			
-			$outp = "[";
 			while ($rs = $results->fetch_array(MYSQLI_ASSOC)) {
-				if ($outp != "[") {
-						$outp .= ",";
-					}
-				$outp .= '{"ID":"' . htmlspecialchars_decode($rs["ID"], ENT_QUOTES) . '",';
-				$outp .= '"display":"' . htmlspecialchars_decode($rs["display"], ENT_QUOTES) . '",';
-				$outp .= '"contentTitle":"' . $rs["contentTitle"] . '",';
-				//$outp .= '"content":"' . htmlspecialchars(json_encode($rs["content"]), ENT_QUOTES, 'UTF-8') . '"}';
-				$outp .= '"content":"' . base64_encode(htmlspecialchars_decode($rs["content"], ENT_QUOTES)). '",'; 
-				$outp .= '"page":"' . htmlspecialchars($rs["page"], ENT_QUOTES) . '",';
-				$outp .= '"pYear":"' . htmlspecialchars($rs["pYear"], ENT_QUOTES) . '",';
-				$outp .= '"pMonth":"' . htmlspecialchars($rs["pMonth"], ENT_QUOTES) . '",';
-				$outp .= '"pDay":"' . htmlspecialchars($rs["pDay"], ENT_QUOTES) . '",';
-				$outp .= '"dateModified":"' . htmlspecialchars($rs["dateModified"], ENT_QUOTES) . '"}';
+				$json[] = $rs;
 			}
-			$outp .= "]";
-			
 			$this->close();
-			return $outp;
+			return json_encode($json);
 		}
 		
 	}
